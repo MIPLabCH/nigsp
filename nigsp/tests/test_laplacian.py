8ca8a150c5e4e95ee1ef5668d6e703678f77108b
smoia <s.moia.research@gmail.com>
2022-04-15 17:44:58 +0200

    Test operation.laplacian

--- nigsp/tests/test_laplacian.py
+++ nigsp/tests/test_laplacian.py
@@ -1,11 +1,40 @@
 #!/usr/bin/env python3
 """Tests for operations.laplacian."""
+from os.path import remove
 
-from pytest import mark, raises
+import numpy as np
 
 from nigsp.operations import laplacian
 
 
 # ### Unit tests
+def test_symmetric_norm(sc_mtx):
+
+    d = np.diag(sc_mtx.sum(axis=-1) ** (-1/2))
+
+    symm_norm = (d @ sc_mtx @ d)
+
+    lapl_in = np.eye(sc_mtx.shape[0]) - symm_norm
+    lapl_out = laplacian.symmetric_normalisation(sc_mtx)
+
+    assert lapl_in == lapl_out
+
+    remove(sc_mtx)
+
+
+def test_decomposition(sc_mtx):
+
+    eival, eivec = np.linalg.eig(sc_mtx)
+
+    idx = np.argsort(eival)
+    eival = eival[idx]
+    # #!# Check that eigenvec has the right index and not inverted 
+    eivec = eivec[:, idx]
+
+    eigenval, eigenvec = laplacian.decomposition(sc_mtx)
+
+    assert (eival == eigenval).all()
+    assert (eivec == eigenvec).all()
+
 
 # ### Break tests
