ca221a3cdd91d801983e02123090250003f10cef
smoia <s.moia.research@gmail.com>
2022-07-18 10:30:10 +0200

    fix: Fix general import to avoid test import

--- nigsp/__init__.py
+++ nigsp/__init__.py
@@ -5,12 +5,14 @@ import pkgutil
 from ._version import get_versions
 from .operations import graph, laplacian, metrics, nifti, surrogates, timeseries
 
+SKIP_MODULES = ['tests']
 
 __version__ = get_versions()['version']
 del get_versions
 
 __all__ = []
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    _module = loader.find_module(module_name).load_module(module_name)
-    globals()[module_name] = _module
+    if module_name not in SKIP_MODULES:
+        __all__.append(module_name)
+        _module = loader.find_module(module_name).load_module(module_name)
+        globals()[module_name] = _module
