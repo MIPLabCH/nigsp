2a77fb5240160379bba4be09ed3ac195be692f4b
Stefano Moia <s.moia@bcbl.eu>
2022-11-04 01:26:15 +0100

    Try again

--- nigsp/__init__.py
+++ nigsp/__init__.py
@@ -1,20 +1,18 @@
 """Hopefully importing everything."""
 
 import pkgutil
-from os.path import dirname, abspath
 
 from ._version import get_versions
 from .operations import graph, laplacian, metrics, nifti, surrogates, timeseries
+
+SKIP_MODULES = ["tests"]
 
 __version__ = get_versions()["version"]
 del get_versions
 
 __all__ = []
-
-path = dirname(abspath(__file__))
-
-# for loader, module_name, is_pkg in pkgutil.walk_packages(path):
-#     if "tests" not in module_name:
-#         __all__.append(module_name)
-#         _module = loader.find_module(module_name).load_module(module_name)
-#         globals()[module_name] = _module
+for loader, module_name, is_pkg in pkgutil.walk_packages(__name__.__path__):
+    if "tests" not in module_name:
+        __all__.append(module_name)
+        _module = loader.find_module(module_name).load_module(module_name)
+        globals()[module_name] = _module
