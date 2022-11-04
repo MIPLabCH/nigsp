f3c8d38e604214be6b5ba38ce98c734e5f64d7f6
Stefano Moia <s.moia@bcbl.eu>
2022-11-04 01:29:31 +0100

    At least the test import is fixed

--- nigsp/__init__.py
+++ nigsp/__init__.py
@@ -11,7 +11,7 @@ __version__ = get_versions()["version"]
 del get_versions
 
 __all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__name__.__path__):
+for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
     if "tests" not in module_name:
         __all__.append(module_name)
         _module = loader.find_module(module_name).load_module(module_name)
