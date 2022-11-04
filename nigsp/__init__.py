60588f2c8a4fc60f85dab720b4033a324039087e
Stefano Moia <s.moia@bcbl.eu>
2022-11-04 01:15:18 +0100

    Fix nigsp init

--- nigsp/__init__.py
+++ nigsp/__init__.py
@@ -1,18 +1,20 @@
 """Hopefully importing everything."""
 
 import pkgutil
+from os.path import dirname, abspath
 
 from ._version import get_versions
 from .operations import graph, laplacian, metrics, nifti, surrogates, timeseries
-
-SKIP_MODULES = ["tests"]
 
 __version__ = get_versions()["version"]
 del get_versions
 
 __all__ = []
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    if module_name not in SKIP_MODULES:
+
+path = dirname(abspath(__file__))
+
+for loader, module_name, is_pkg in pkgutil.walk_packages(path):
+    if "tests" not in module_name:
         __all__.append(module_name)
         _module = loader.find_module(module_name).load_module(module_name)
         globals()[module_name] = _module
