06aac785e4cc361b87ce3a96373b4eb04410baa3
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-08-16 15:55:10 +0200

    rm ref to css files for sphinx-gallery

--- docs/conf.py
+++ docs/conf.py
@@ -100,9 +100,6 @@ html_show_sphinx = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
 html_static_path = ["_static"]
-html_css_files = [
-    "css/style.css",
-]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
