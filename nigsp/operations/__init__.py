8765dacdccddba14d0918cef576770207259fc73
smoia <s.moia.research@gmail.com>
2023-10-11 00:33:29 +0200

    Update init of operations

--- nigsp/operations/__init__.py
+++ nigsp/operations/__init__.py
@@ -14,13 +14,23 @@ For example, calling `nigsp.operations.metrics.sdi` is equivalent to calling
 
 # Import all operations.
 from .graph import nodestrength, zerocross
-from .laplacian import decomposition, symmetric_normalised_laplacian
+from .laplacian import (
+    compute_laplacian,
+    decomposition,
+    normalisation,
+    recomposition,
+    symmetric_normalised_laplacian,
+)
 from .metrics import functional_connectivity, gsdi, sdi
 from .nifti import apply_atlas, apply_mask, mat_to_vol, unfold_atlas, unmask, vol_to_mat
 from .surrogates import random_sign, sc_informed, sc_uninformed, test_significance
 from .timeseries import (
+    demean_ts,
     graph_filter,
     graph_fourier_transform,
     median_cutoff_frequency_idx,
     normalise_ts,
+    rescale_ts,
+    resize_ts,
+    spc_ts,
 )
