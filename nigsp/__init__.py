ac48dacc5fe5eec58e74394794be1a3f8c88e774
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-03-12 11:44:00 +0100

    rm unused variable

--- nigsp/__init__.py
+++ nigsp/__init__.py
@@ -14,7 +14,5 @@ from ._version import get_versions
 from .cli import run
 from .operations import graph, laplacian, metrics, nifti, surrogates, timeseries
 
-SKIP_MODULES = ["tests"]
-
 __version__ = get_versions()["version"]
 del get_versions
