8cf1d755db778628fd8121a7cbc170dc0870e22d
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-03-12 10:58:55 +0100

    fix module discovery logic

--- nigsp/__init__.py
+++ nigsp/__init__.py
@@ -12,7 +12,9 @@ del get_versions
 
 __all__ = []
 for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    if "tests" not in module_name or "conftest" not in module_name:
+    if module_name == "conftest":
+        continue
+    if "tests" not in module_name:
         __all__.append(module_name)
         _module = loader.find_module(module_name).load_module(module_name)
         globals()[module_name] = _module
