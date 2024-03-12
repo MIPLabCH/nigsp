4c590987f655cbf4a09600afed0ac1c268256bc3
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-03-12 11:43:10 +0100

    fix missed imports

--- nigsp/__init__.py
+++ nigsp/__init__.py
@@ -1,5 +1,17 @@
-from . import blocks, due, io, objects, references, utils, viz, workflow
+from . import (
+    blocks,
+    cli,
+    due,
+    io,
+    objects,
+    operations,
+    references,
+    utils,
+    viz,
+    workflow,
+)
 from ._version import get_versions
+from .cli import run
 from .operations import graph, laplacian, metrics, nifti, surrogates, timeseries
 
 SKIP_MODULES = ["tests"]
