b55bc0c7e04122ab9ded8e3d454a5ad04663c8d5
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-22 14:43:34 +0200

    suppress sphinx gallery warning

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,9 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 nitpicky = True
 nitpick_ignore = []
 
+# list of warning types to suppress
+suppress_warnings = ["config.cache"]
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
